b60692f68ae087629eb5c418908469f04dbc43dd
Brendon Cambuí <brendongouveiacambui@hotmail.com>
2020-04-23 12:55:08 -0300

    Update file.ts

--- src/lib/file.ts
+++ src/lib/file.ts
@@ -3,3 +3,10 @@ import { promisify } from "util"
 
 export const readFile = promisify(fs.readFile)
 export const writeFile = promisify(fs.writeFile)
+export const exists = promisify(fs.exists)
+export const createDirectory = promisify(fs.mkdir)
+
+export const createDirectoryIfNotExists = async (path: string) => {
+  if (!await exists(path))
+    await createDirectory(path)
+}
